3af97cd91de39119daa3a96e7843a45ed593dec6
Sam <samagraham2003@gmail.com>
2022-04-04 01:41:02 -0700

    auth/deauthorize + fetchData standalone functions

--- api/api.py
+++ api/api.py
@@ -17,34 +17,66 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
+CREDENTIALS_DIR = os.path.dirname(os.path.realpath(__file__)) + '\\' + 'credentials.json'
+TOKEN_DIR = os.path.dirname(os.path.realpath(__file__)) + '\\' + 'token.json'
+
+PORT=8000
+
 app = Flask(__name__)
 
-@app.route('/OAuth')
-def authenticate():
-    creds = None
-    if os.path.exists(os.path.dirname(os.path.realpath(__file__)) + '\\' + 'token.json'):
-        creds = Credentials.from_authorized_user_file(os.path.dirname(os.path.realpath(__file__)) + '\\' + 'token.json', SCOPES)
+@app.route('/authStatus')
+def get_auth_status():
+    return {'authorized': os.path.exists(TOKEN_DIR)}
+
+@app.route('/deauthorize')
+def deauthorize():
+    if os.path.exists(TOKEN_DIR):
+        os.remove(TOKEN_DIR)
+    return {'authorized': os.path.exists(TOKEN_DIR)}
+
+@app.route('/authorize')
+def authorize():
+    creds = get_creds()
+    # if os.path.exists(TOKEN_DIR):
+    #     creds = Credentials.from_authorized_user_file(TOKEN_DIR, SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(os.path.dirname(os.path.realpath(__file__)) + '\\' + 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8000)
-        with open(os.path.dirname(os.path.realpath(__file__)) + '\\' + 'token.json', 'w') as token:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_DIR, SCOPES)
+            creds = flow.run_local_server(port=PORT)
+            # may be able to access the redirect url and handle it for return to lh3000
+
+        with open(TOKEN_DIR, 'w') as token:
             token.write(creds.to_json())
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-        if not values:
-            print('No data found.')
-            return
-        print('Name, Major:')
-        for row in values:
-            print('%s, %s' % (row[0], row[4]))
-        # return {'OAuth': 'no exceptions'}
-        return {'OAuth': values}
-    except HttpError as err:
-        print(err)
-        return {'OAuth': 'HttpError'}
+    return {'authorized': os.path.exists(TOKEN_DIR)}
+
+
+@app.route('/fetchData')
+def fetch_data():
+    creds = get_creds()
+    if creds == None:
+        return {'fetchData': 'Not Authorized'}
+    else:
+        try:
+            service = build('sheets', 'v4', credentials=creds)
+            sheet = service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+            values = result.get('values', [])
+            # if not values:
+            #     print('No data found.')
+            #     return
+            # print('Name, Major:')
+            # for row in values:
+            #     print('%s, %s' % (row[0], row[4]))
+            return {'fetchData': values}
+        except HttpError as err:
+            # print(err)
+            return {'fetchData': 'HTTP Error: ' + err}
+
+def get_creds():
+    creds = None
+    if os.path.exists(TOKEN_DIR):
+        creds = Credentials.from_authorized_user_file(TOKEN_DIR, SCOPES)
+    return creds
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [authorized, setAuthorized] = useState(null);
   const [oAuthData, setOAuthData] = useState(null);
 
-  const oAuthFetch = () => {
-    fetch('/OAuth')
-      .then(res => res.json())
-      .then(data => {
-        setOAuthData(data.OAuth);
-      });
+  useEffect(() => {
+    fetch('/authStatus')
+    .then(res => res.json())
+    .then(data => {
+      setAuthorized(data.authorized);
+    });
+  }, []);
+
+  const fetchData = () => {
+    fetch('/fetchData')
+    .then(res => res.json())
+    .then(data => {
+      setOAuthData(data.fetchData);
+    });
+  }
+
+  const authorize = () => {
+    fetch('/authorize')
+    .then(res => res.json())
+    .then(data => {
+      setAuthorized(data.authorized);
+    });
+  }
+
+  const deauthorize = () => {
+    fetch('/deauthorize')
+    .then(res => res.json())
+    .then(data => {
+      setAuthorized(data.authorized);
+    });
   }
   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => oAuthFetch()}>OAuth Button</button>
+        {authorized === false && <button onClick={() => authorize()}>Authorize</button>}
+        {authorized === true && <button onClick={() => deauthorize()}>Deauthorize</button>}
+        <button onClick={() => fetchData()}>Fetch Data</button>
         <br></br>
         OAuth Data:
         <p>{oAuthData}</p>
